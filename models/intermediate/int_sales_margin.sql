64e6a0a46796f64eac085f2e5d279f03c8144592
Benoit Cattanéo <benoit.cattaneo@gmail.com>
2025-11-05 13:35:08 +0000

    OK Test

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,29 +1,15 @@
-WITH sales AS (
-  SELECT
-    orders_id,
-    products_id,
-    quantity,
-    revenue
-  FROM  ref('stg_gz_raw_data__sales') 
-),
-product AS (
-  SELECT
-    products_id,
-    purchase_price
-  FROM  ref('stg_gz_raw_data__product') 
-)
-SELECT
-  s.orders_id,
-  s.products_id,
-  s.quantity,
-  s.revenue,
-  p.purchase_price,
-  s.quantity * p.purchase_price AS purchase_cost,
-  s.revenue - (s.quantity * p.purchase_price) AS margin,
-  SAFE_DIVIDE(
-    s.revenue - (s.quantity * p.purchase_price),
-    NULLIF(s.quantity * p.purchase_price, 0)
-  ) AS margin_rate
-FROM sales s
-INNER JOIN product p
-  ON p.products_id = s.products_id
+-- models/intermediate/int_sales_margin.sql
+select
+  products_id,
+  date_date,
+  orders_id,
+  revenue,
+  quantity,
+  purchase_price,
+  round(s.quantity * p.purchase_price, 2) as purchase_cost,
+  round(s.revenue - s.quantity * p.purchase_price, 2) as margin
+from  ref('stg_gz_raw_data__sales')  as s
+left join  ref('stg_gz_raw_data__product')  as p
+  using (products_id)
+
+  
--- models/staging/gz_raw_data/stg_gz_raw_data__product.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__product.sql
@@ -1,21 +1,15 @@
-with 
+-- models/staging/gz_raw_data/stg_gz_raw_data__product.sql
+ config(alias='stg_raw_data__product') 
 
-source as (
-
-    select * from {{ source('gz_raw_data', 'product') }}
-
+with source as (
+  select *
+  from  source('gz_raw_data', 'product') 
 ),
-
 renamed as (
-
-    select
-        products_id,
-       CAST(purchse_price AS FLOAT64) AS purchase_price
-
-    from source
-
+  select
+    products_id,
+    cast(purchase_price as float64) as purchase_price
+  from source
 )
-
-select * from renamed
-
- 
+select *
+from renamed
